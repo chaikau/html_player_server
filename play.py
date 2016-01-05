27a4eb216134a3c9171c06fad9b27281c3b39eeb
chaikau <chaikau@163.com>
2016-01-05 16:28:18 +0800

    update

--- play.py
+++ play.py
@@ -1,6 +1,5 @@
 #-*- coding: UTF-8 -*-
 from flask import Flask , url_for, render_template ,request
-import subprocess
 import os, sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -9,8 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    p=subprocess.Popen('ls static/video', shell=True, stdout=subprocess.PIPE)
-    files=p.stdout.readlines()
+    files=os.listdir('static/video')
     if files: # files not empty
         return render_template('index.html', files=files)
     else:
@@ -26,5 +24,5 @@ def play(videofile):
 
 if __name__ == '__main__':
     #app.run(port=8000)
-    app.run(host='0.0.0.0',port=8000 , debug=True)
+    app.run(host='0.0.0.0',port=8000 )
 
